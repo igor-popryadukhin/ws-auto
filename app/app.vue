8957066bc65ae18ed5d06905fef9f6d5a7e32489
Igor <48174353+igor-popryadukhin@users.noreply.github.com>
2025-10-18 13:25:26 +0300

    Fix missing layout components and imports

--- app/app.vue
+++ app/app.vue
@@ -9,3 +9,8 @@
     <AppFooter />
   </div>
 </template>
+
+<script setup lang="ts">
+import AppFooter from '../components/AppFooter.vue'
+import AppHeader from '../components/AppHeader.vue'
+</script>
